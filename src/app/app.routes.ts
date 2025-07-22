994e29f46f32b93ebf2fd9165a561c73433d67f3
apaun <apaun@lenovo.com>
2025-07-22 10:11:21 +0300

    added component structure and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
+import { OrderComponent } from './components/order/order.component';
+import { AdminComponent } from './components/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: '/login', component: LoginComponent},
+    {path: '/home', component: HomeComponent},
+    {path: '/restaurant', component: RestaurantComponent},
+    {path: '/order', component: OrderComponent},
+    {path: '/admin', component: AdminComponent},
+];
---
+++ src/app/components/restaurant/restaurant.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant',
+  imports: [],
+  templateUrl: './restaurant.component.html',
+  styleUrl: './restaurant.component.css'
+})
+export class RestaurantComponent {
+
+}

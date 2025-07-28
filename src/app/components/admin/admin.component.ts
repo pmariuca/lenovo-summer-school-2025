1cc8e127d94812a832b869542f1d3343e60e2486
madam2 <madam2@lenovo.com>
2025-07-28 22:04:31 +0300

    finish order page and start admin page

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrl: './admin.component.css',
 })
-export class AdminComponent {
-
-}
+export class AdminComponent {}
--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -11,6 +11,8 @@ import {
 import { CheckboxModule } from 'primeng/checkbox';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { ButtonModule } from 'primeng/button';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-order',
@@ -22,11 +24,32 @@ import { ButtonModule } from 'primeng/button';
     CheckboxModule,
     ButtonModule,
     RadioButtonModule,
+    OverlayBadgeModule,
+    AvatarModule,
   ],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css',
 })
 export class OrderComponent {
+  orderBasket = [
+    {
+      dishId: '6887544399c44a0944ca2e5e',
+      restaurantId: '6887544399c44a0944ca2e5d',
+      name: 'Beef Tacos',
+      description: 'Corn tortillas filled with seasoned beef and toppings',
+      price: 10,
+      quantity: 1,
+    },
+    {
+      dishId: '6887544399c44a0944ca2e5f',
+      restaurantId: '6887544399c44a0944ca2e5d',
+      name: 'Churros',
+      description: 'Fried dough pastry with cinnamon sugar',
+      price: 6,
+      quantity: 2,
+    },
+  ];
+
   deliveryFormGroup = new FormGroup({
     contact: new FormControl(''),
     firstName: new FormControl('', Validators.required),
@@ -38,5 +61,11 @@ export class OrderComponent {
 
   displayValues() {
     console.log(this.deliveryFormGroup.getRawValue());
+  }
+
+  getTotalValue() {
+    return this.orderBasket.reduce((acc, currV) => {
+      return acc + currV.price * currV.quantity;
+    }, 0);
   }
 }
